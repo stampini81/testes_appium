56fac3b31e6e158f273f929d3fb4cd334e126f69
Leandro da Silva Stampini <stampini81@gmail.com>
2025-09-24 20:03:49 -0300

    teste

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,23 +3,10 @@ require 'selenium-webdriver'
 
 BROWSER = (ENV['BROWSER'] || 'chrome').to_sym
 
-Capybara.register_driver :selenium_remote do |app|
-  capabilities =
-    case BROWSER
-    when :firefox
-      Selenium::WebDriver::Remote::Capabilities.firefox
-    else
-      Selenium::WebDriver::Remote::Capabilities.chrome
-    end
-
-  Capybara::Selenium::Driver.new(
-    app,
-    browser: :remote,
-    url: 'http://localhost:4444/wd/hub',
-    capabilities: capabilities
-  )
+Capybara.register_driver :selenium do |app|
+  Capybara::Selenium::Driver.new(app, browser: BROWSER)
 end
 
-Capybara.default_driver = :selenium_remote
-Capybara.javascript_driver = :selenium_remote
+Capybara.default_driver = :selenium
+Capybara.javascript_driver = :selenium
 Capybara.default_max_wait_time = 10
